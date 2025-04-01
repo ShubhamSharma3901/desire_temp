7d480a2eef68e8bcfc1e9bd6643cf2e22abc9eab
Shubham Sharma <desire.itdpt@gmail.com>
2025-04-01 11:28:53 +0530

    timeline heading added

--- components/About/Timeline.tsx
+++ components/About/Timeline.tsx
@@ -10,6 +10,7 @@ import {
   TimelineTime,
   TimelineTitle,
 } from "flowbite-react";
+import React from "react";
 
 const TimelineComponent = () => {
   const timelineData = [
@@ -24,6 +25,12 @@ const TimelineComponent = () => {
 
   return (
     <>
+      <div className="flex justify-start items-center w-full gap-5 font-titilium mb-14">
+        <p className="smTablet:text-[min(3vw,3vh)] xsPhone:text-[min(5vw,5vh)] font-light text-neutral-800">
+          Company Timeline
+        </p>
+        <hr className="relative   right-0 h-1 w-[100px] bg-[#085C2C]" />
+      </div>
       <Timeline horizontal={true} className={"tablet:flex xsPhone:hidden"}>
         {timelineData.map((event, index) => (
           <TimelineItem key={index}>
